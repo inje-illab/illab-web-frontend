869e1fbfbd5109b8c5defe5d293fea8f18f71209
eona1301 <eona1301@gmail.com>
2023-03-25 04:25:37 +0900

    #8 🚚 style: main box rename to main title box

--- src/information/components/main/MainBox.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const MainBox = ({text}) => {
-    return (
-        <div className="text-2xl font-bold">{text}</div>
-    );
-};
-
-export default MainBox;
---
+++ src/information/components/main/MainTitleBox.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const MainBox = ({text, className}) => {
+    return (
+        <div className={className}>{text}</div>
+    );
+};
+
+export default MainBox;
