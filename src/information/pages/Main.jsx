cbe4067c43cb9a513bf95b89e94f2b5e60433dc9
eona1301 <eona1301@gmail.com>
2023-03-25 05:09:36 +0900

    #8 🔨 feat: update information main page
    - create 3 content
    - apply responsive

--- src/information/pages/Main.jsx
+++ src/information/pages/Main.jsx
@@ -1,26 +1,37 @@
 import React from 'react';
 import LaptopTable from '../../common/image/LaptopTable';
-import MainBox from '../components/main/MainBox';
+import MainBottomBox from '../components/main/MainBottomBox';
+import MainBottomMobileBox from '../components/main/MainBottomMobileBox';
+import MainMiddleBox from '../components/main/MainMiddleBox';
+import MainMiddleMobileBox from '../components/main/MainMiddleMobileBox';
+import MainTitleBox from '../components/main/MainTitleBox';
+import MainTopBox from '../components/main/MainTopBox';
+import MainTopMobileBox from '../components/main/MainTopMobileBox';
 
 const Main = () => {
     const mainIntroText = [
-        {text : "새로운 아이디어와 적극적인 개발자 소통,", className:"text-[1rem]"},
-        {text : "프로젝트 기반의 협업 학습 연구실", className:"text-xl mb-20"},
+        {text : "새로운 아이디어와 적극적인 개발자 소통,", className:"text-2xl"},
+        {text : "프로젝트 기반의 협업 학습 연구실", className:"text-2xl mb-20"},
         {text :"Innocative Learning Laboratory", className:"text-5xl font-extrabold"}
     ]
     const mainIntroMdText = [
-        {text : "새로운 아이디어와 적극적인 개발자 소통,", className:"text-[1rem]"},
-        {text : "프로젝트 기반의 협업 학습 연구실", className:"text-sm mb-3"},
+        {text : "새로운 아이디어와 적극적인 개발자 소통,", className:"text-xs"},
+        {text : "프로젝트 기반의 협업 학습 연구실", className:"text-xs mb-3"},
         {text : "Innocative Learning Laboratory", className:"text-xl font-bold"}
     ]
     const mainContent = [
-        {text : <MainBox text={"📖 illab은 _____ 을 자랑합니다."}/>, content : ""},
-        {text : <MainBox text={"🙌🏻  illab은 _____ 분위기를 지향합니다."}/>, content : ""},
-        {text : <MainBox text={"💻  illab은 _____ 학습 및 활동을 합니다."}/>, content : ""}
+        {text : <MainTitleBox text={"📖 illab은 _____ 을 자랑합니다."} className="font-bold text-2xl"/>, content : <MainTopBox/>},
+        {text : <MainTitleBox text={"🙌🏻 illab은 _____ 분위기를 지향합니다."} className="font-bold text-2xl"/>, content : <MainMiddleBox/>},
+        {text : <MainTitleBox text={"💻 illab은 _____ 학습 및 활동을 합니다."} className="font-bold text-2xl"/>, content : <MainBottomBox/>}
+    ]
+    const mainMobileContent = [
+        {text : <MainTitleBox text={"📖 illab은 _____ 을 자랑합니다."} className="font-bold"/>, content : <MainTopMobileBox className="text-xs"/>},
+        {text : <MainTitleBox text={"🙌🏻 illab은 _____ 분위기를 지향합니다."} className="font-bold"/>, content : <MainMiddleMobileBox/>},
+        {text : <MainTitleBox text={"💻 illab은 _____ 학습 및 활동을 합니다."} className="font-bold"/>, content : <MainBottomMobileBox/>}
     ]
     return (
         <div>
-            {/* lg 이상 */}
+            {/* lg 이상 메인 이미지*/}
             <div className="hidden lg:block">
                 <div className="relative -z-50 flex justify-center">
                         <LaptopTable className={'z-0 backdrop-brightness-95'}/>
@@ -29,7 +40,7 @@ const Main = () => {
                     </div>
                 </div>
             </div>
-            {/* lg 미만 */}
+            {/* lg 미만 메인 이미지*/}
             <div className='lg:hidden'>
                 <div className="relative -z-50 flex justify-center">
                         <LaptopTable className={'z-0'}/>
@@ -38,10 +49,16 @@ const Main = () => {
                     </div>
                 </div>
             </div>
-            
-            <div className="max-w-6xl m-auto">
+            {/* md 이상 메인 콘텐츠 */}
+            <div className="hidden md:block max-w-6xl m-auto py-12">
                 <div className="text-md">
-                    {mainContent.map((contents, index)=>(<div key={index} className="p-10">{contents.text}{contents.content}</div>))}
+                    {mainContent.map((contents, index)=>(<div key={index}>{contents.text}{contents.content}</div>))}
+                </div>
+            </div>
+            {/* md 미만 메인 콘텐츠 */}
+            <div className="md:hidden m-auto p-5">
+                <div className="text-md">
+                    {mainMobileContent.map((contents, index)=>(<div key={index}>{contents.text}{contents.content}</div>))}
                 </div>
             </div>
         </div>
